b4c7c0389e12264aa1e27285d53712414c7d75b0
Hayatimiz Oyun <mertdundaroyunda@gmail.com>
2026-05-22 01:27:19 +0300

    V2.5.1 Fix 5 admin tone ui

--- api/health.js
+++
@@ -1,2 +0,0 @@
-const { json, getSupabase } = require('../lib/_lib');
-module.exports = async (req,res)=>{try{const s=getSupabase();const {error}=await s.from('site_settings').select('id').eq('id',1).single();json(res,error?500:200,{ok:!error,supabase:!error,message:error?error.message:'Sistem çalışıyor'});}catch(e){json(res,500,{ok:false,message:e.message});}};
